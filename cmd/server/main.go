dfad685471d0d1a77b85401919a87eeb830a4760
2824920336 <2824920336@qq.com>
2026-01-25 10:51:01 +0800

    更新了中间件的管理

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"rent/internal/config"
 	"rent/internal/db"
+	"rent/internal/middleware"
 	"rent/internal/router"
 )
 
@@ -13,24 +13,10 @@ func main() {
 	mysqlDB := db.InitMySQL()
 	redisDB := db.InitRedis()
 	r := gin.Default()
-	r.Use(CorsMiddleware())
+	r.Use(middleware.CorsMiddleware())
+	r.Use(gin.Recovery())
 	router.InitRouter(r, mysqlDB, redisDB)
-	r.Run(":8080")
-}
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		c.Header("Access-Control-Expose-Headers", "Content-Length")
-		c.Header("Access-Control-Allow-Credentials", "true")
-
-		// 处理预检请求
-		if c.Request.Method == http.MethodOptions {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
-		c.Next()
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
 	}
 }
